73acdf9d47915ae86e9d8e4ffed1748669a6d514
WangShuaiPeng0824 <2926914793@qq.com>
2020-06-04 20:49:42 +0800

    ElasticSearch入门案例

--- springboot_elasticsearch/src/test/java/com/usian/test/IndexWriterTest.java
+++ springboot_elasticsearch/src/test/java/com/usian/test/IndexWriterTest.java
@@ -5,7 +5,14 @@ import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.IndicesClient;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -73,6 +80,45 @@ public class IndexWriterTest {
         //restHighLevelClient.indices()：返回索引库客户端
         DeleteIndexResponse response = restHighLevelClient.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
         System.out.println(response.isAcknowledged());
-
     }
+
+    @Test
+    public void testAddDoc() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("java1906","course","1");
+        indexRequest.source("{\n" +
+                "  \"name\": \"spring cloud实战\",\n" +
+                "  \"description\": \"本课程主要从四个章节进行讲解： 1.微服务架构入门 2.spring cloud 基础入门 3.实战Spring Boot 4.注册中心eureka。\",\n" +
+                "  \"studymodel\": \"201001\",\n" +
+                "  \"price\": 5.6\n" +
+                "}",XContentType.JSON);
+        IndexResponse response = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
+    }
+
+    @Test
+    public void testBulkAddDoc() throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.add(new IndexRequest("java1906","course").source("{\"name\":\"php实战\",\"description\":\"php谁都不服\",\"studymodel\":\"201001\",\"price\":\"5.6\"}",XContentType.JSON));
+        bulkRequest.add(new IndexRequest("java1906","course").source("{\"name\":\"net实战\",\"description\":\"net从入门到放弃\",\"studymodel\":\"201001\",\"price\":\"7.6\"}",XContentType.JSON));
+        BulkResponse response = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println(response.hasFailures());
+    }
+
+    @Test
+    public void testUpdateDoc() throws IOException {
+        UpdateRequest updateRequest = new UpdateRequest("java1906","course","1");
+        updateRequest.doc("{\n" +
+                " \"price\":7.77\n" +
+                "}",XContentType.JSON);
+        UpdateResponse response = restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
+    }
+
+    @Test
+    public void testDelDoc() throws IOException {
+        DeleteRequest deleteRequest = new DeleteRequest("java1906", "course", "1");
+        DeleteResponse response = restHighLevelClient.delete(deleteRequest, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
+    }
+
 }
